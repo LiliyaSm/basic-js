c2d3d1f2273e933b4f5c422dccc5fa2a334fc9c4
Liliia Smelova <liliia.smelova@outlook.com>
2020-09-13 11:40:30 +0300

    feat: 1 and 3 test passed in recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor() {
+        this.depth = 0;
+        // this.flag = 0;
+    }
+
+    calculateDepth(array) {
+        console.log(array);
+
+        this.depth++;
+        let level = this.depth;
+        console.log("depth0", this.depth);
+        console.log("level", level);
+
+        for (array.value of array) {
+            if (Array.isArray(array.value)) {
+                console.log(array.value);
+                console.log("depth", this.depth);
+                // level++;
+                // if (level > this.depth) {
+                    // this.depth++;
+                    this.calculateDepth(array.value);
+                // }
+                // flag = 1;
+            }
+        }
+        if (level === 1) {
+        let result = this.depth;
+        this.depth = 0;
+        // console.log("result", result)
+            return result;
+        }
+    }
 };
